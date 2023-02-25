3b12200cd7303029682d6ff8c08f9b0e6fe294d3
lucaso-silva <luc.osilva@gmail.com>
2023-02-25 02:10:55 -0300

    style: (wip) more-info section and improvements on layout responsivity

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// const url = "https://api.rebrandly.com/v1/account";
+
+// async function getInformation() {
+//   const response = await fetch(url);
+
+//   console.log(response);
+
+//     const data = await response.json();
+//     console.log(data);
+
+// }
+
+// getInformation();
